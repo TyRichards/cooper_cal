573283bad869fb89cfe2419c89594d18ac0958f4
Ty Richards <me@tyrichards.com>
2014-12-03 18:47:27 -0600

    add launch page

---
+++ wp-content/themes/Paradox/page-launch.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Launch
+*/
+?>
+<div style="text-align:center; margin-top:200px;">
+<h1>Cooper Calisthenics</h1>
+<p>Coming Soon</p>
+<p>Visit Ryan Cooper on <a href="http://instagram.com/cooper_calisthenics">Instagram</a></p>
+</div>
